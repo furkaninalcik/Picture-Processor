998cf400e89a89e0c2bb4e9f66d8abc0045cc6f7
furkaninalcik <m.furkaninalcik@gmail.com>
2018-06-17 00:08:34 +0300

    0

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var express = require('express');
 
+var bodyParser = require('body-parser')
+
 var app = express();
 
 app.set('view engine','ejs');
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+
 
 
 app.get('/',function(req, res){
